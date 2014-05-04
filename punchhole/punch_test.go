5d748892902876c00b115d702ce56763e75a8afc
Tamás Gulácsi <tgulacsi78@gmail.com>
2014-05-04 13:07:32 +0200

    punchhole: add more tests

--- punchhole/punch_test.go
+++ punchhole/punch_test.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -75,4 +76,52 @@ func (cw *countWriter) Write(p []byte) (int, error) {
 	n, err := cw.Writer.Write(p)
 	cw.n += int64(n)
 	return n, err
+}
+
+func TestPunch(t *testing.T) {
+	if PunchHole == nil {
+		t.Logf("No punchHole implementation is available, skipping.")
+		t.Skip()
+	}
+	file, err := ioutil.TempFile("", "punchhole-")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.Remove(file.Name())
+	defer file.Close()
+	buf := make([]byte, 10<<20)
+	for i := range buf {
+		buf[i] = byte(1 + (i+1)&0xfe)
+	}
+	if _, err = file.Write(buf); err != nil {
+		t.Errorf("error writing to the temp file: %v", err)
+		t.FailNow()
+	}
+	if err = file.Sync(); err != nil {
+		t.Logf("error syncing %q: %v", file.Name(), err)
+	}
+	for i, j := range []int{1, 31, 1 << 10} {
+		if err = PunchHole(file, int64(j), int64(j)); err != nil {
+			t.Errorf("%d. error punching at %d, size %d: %v", i, j, j, err)
+			continue
+		}
+		// read back, with 1-1 bytes overlaid
+		n, err := file.ReadAt(buf[:j+2], int64(j-1))
+		if err != nil {
+			t.Errorf("%d. error reading file: %v", err)
+			continue
+		}
+		buf = buf[:n]
+		if buf[0] == 0 {
+			t.Errorf("%d. file at %d has been overwritten with 0!", i, j-1)
+		}
+		if buf[n-1] == 0 {
+			t.Errorf("%d. file at %d has been overwritten with 0!", i, j-1+n)
+		}
+		for k, v := range buf[1 : n-1] {
+			if v != 0 {
+				t.Errorf("%d. error reading file at %d got %d, want 0.", i, k, v)
+			}
+		}
+	}
 }
