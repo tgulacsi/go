06c049050b706f53a5d452c2af162970d1fa0650
Tamás Gulácsi <tgulacsi78@gmail.com>
2014-12-14 17:59:14 +0100

    gloghlp: better formatting

--- loghlp/gloghlp/gloghlp.go
+++ loghlp/gloghlp/gloghlp.go
@@ -14,17 +14,53 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package gloghlp is a wrap against glog flag set.
 package gloghlp
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"runtime"
+	"strings"
+
 	"github.com/golang/glog"
 	"gopkg.in/inconshreveable/log15.v2"
 )
 
 // GLogHandler returns a log15.Handler which logs using glog.
 func GLogHandler() log15.Handler {
-	return glogHandler{log15.LogfmtFormat()}
+	return glogHandler{GLogfmtFormat()}
+}
+
+func GLogfmtFormat() log15.Format {
+	return log15.FormatFunc(func(r *log15.Record) []byte {
+		_, file, line, ok := runtime.Caller(7) // It's always the same number of frames to the user's call.
+		if !ok {
+			file = "???"
+			line = 1
+		} else {
+			slash := strings.LastIndex(file, "/")
+			if slash >= 0 {
+				file = file[slash+1:]
+			}
+		}
+		if line < 0 {
+			line = 0 // not a real line number, but acceptable to someDigits
+		}
+		var buf bytes.Buffer
+		_, _ = fmt.Fprintf(&buf, "[%s:%d] %s", file, line, r.Msg)
+		if len(r.Ctx) == 0 {
+			return buf.Bytes()
+		}
+		_, _ = io.WriteString(&buf, "; ")
+		for i := 0; i < len(r.Ctx)-1; i++ {
+			if i > 0 {
+				_ = buf.WriteByte(' ')
+			}
+			fmt.Fprintf(&buf, "%s=%q", r.Ctx[i], r.Ctx[i+1])
+		}
+		return buf.Bytes()
+	})
 }
 
 type glogHandler struct {
@@ -32,8 +68,7 @@ type glogHandler struct {
 }
 
 func (gl glogHandler) Log(r *log15.Record) error {
-	b := gl.fmt.Format(r)
-	s := string(b[:len(b)-1]) // strip \n
+	s := string(gl.fmt.Format(r)) // strip \n
 	switch r.Lvl {
 	case log15.LvlCrit:
 		glog.Fatal(s)
