d0a8edfe494de430603217dd2f6726fa9fbacbe9
Tamás Gulácsi <tgulacsi78@gmail.com>
2015-05-06 16:31:54 +0200

    add ClearName

---
+++ promhlp/name.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2015 Tamás Gulácsi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package promhlp
+
+import "strings"
+
+// ClearName replaces non-allowed characters.
+func ClearName(txt string, allowColon bool, replaceRune rune) string {
+	if replaceRune == 0 {
+		replaceRune = -1
+	}
+	i := -1
+	return strings.Map(
+		func(r rune) rune {
+			i++
+			if r == '.' {
+				return '_'
+			}
+			if 'a' <= r && r <= 'z' || 'A' <= r && r <= 'Z' || r == '_' {
+				return r
+			}
+			if allowColon && r == ':' {
+				return r
+			}
+			if i > 0 && '0' <= r && r <= '9' {
+				return r
+			}
+			return replaceRune
+		},
+		txt)
+}
