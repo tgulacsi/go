c4bb7d549371a383233ea6d692eb9e148ead1b27
Tamás Gulácsi <tgulacsi78@gmail.com>
2015-11-15 07:34:13 +0100

    add httperr

---
+++ httperr/httperr.go
@@ -0,0 +1,48 @@
+// Copyright 2015 Tamás Gulácsi. All rights reserved.
+// Use of this source code is governed by an Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Package httperr provides a small helper for returning statuscode-accompanied errors.
+package httperr
+
+import (
+	"net/http"
+
+	"gopkg.in/errgo.v1"
+)
+
+var _ = error(httpError{})
+
+type httpError struct {
+	error
+	code int
+}
+
+// Code returns the error's accompanied StatusCode.
+func (he httpError) Code() int {
+	return he.code
+}
+func (he httpError) Cause() error {
+	return he.error
+}
+
+// New returns a new error with the specified error and StatusCode.
+// If code is zero, http.StatusInternalServerError is used.
+// If err is nil, a new error is created from the status code.
+func New(err error, code int) httpError {
+	if code == 0 {
+		code = http.StatusInternalServerError
+	}
+	if err == nil {
+		err = errgo.Newf(http.StatusText(code))
+	}
+	return httpError{error: err, code: code}
+}
+
+// Newf returns a new httpError with the given code,
+// and the message is created from msg and the args.
+func Newf(code int, msg string, args ...interface{}) httpError {
+	return New(errgo.Newf(msg, args), code)
+}
+
+// vim: fileencoding=utf-8:
