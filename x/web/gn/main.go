65d201b86458be1ec56a1f4a4bb8512a57d2db39
Tamás Gulácsi <tgulacsi78+acer@gmail.com>
2016-03-04 15:24:48 +0100

    add gin example

---
+++ x/web/gn/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"math/rand"
+
+	"github.com/gin-gonic/gin"
+)
+
+var html = template.Must(template.ParseGlob("*.html"))
+
+func main() {
+	router := gin.Default()
+	router.SetHTMLTemplate(html)
+
+	router.GET("/room/:roomid", roomGET)
+	router.POST("/room/:roomid", roomPOST)
+	router.DELETE("/room/:roomid", roomDELETE)
+	router.GET("/stream/:roomid", stream)
+
+	router.Run(":8081")
+}
+
+func stream(c *gin.Context) {
+	roomid := c.Param("roomid")
+	listener := openListener(roomid)
+	defer closeListener(roomid, listener)
+
+	c.Stream(func(w io.Writer) bool {
+		c.SSEvent("message", <-listener)
+		return true
+	})
+}
+
+func roomGET(c *gin.Context) {
+	roomid := c.Param("roomid")
+	userid := fmt.Sprint(rand.Int31())
+	c.HTML(200, "chat_room.html", gin.H{
+		"roomid": roomid,
+		"userid": userid,
+	})
+}
+
+func roomPOST(c *gin.Context) {
+	roomid := c.Param("roomid")
+	userid := c.PostForm("user")
+	message := c.PostForm("message")
+	room(roomid).Submit(userid + ": " + message)
+
+	c.JSON(200, gin.H{
+		"status":  "success",
+		"message": message,
+	})
+}
+
+func roomDELETE(c *gin.Context) {
+	roomid := c.Param("roomid")
+	deleteBroadcast(roomid)
+}
