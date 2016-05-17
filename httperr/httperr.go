cea3b05aa57b822e447050ad7ab5061daf41be21
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-05-17 11:26:35 +0200

    httperr: return pointer

--- httperr/httperr.go
+++ httperr/httperr.go
@@ -29,19 +29,28 @@ func (he httpError) Cause() error {
 // New returns a new error with the specified error and StatusCode.
 // If code is zero, http.StatusInternalServerError is used.
 // If err is nil, a new error is created from the status code.
-func New(err error, code int) httpError {
+func New(err error, code int) *httpError {
 	if code == 0 {
-		code = http.StatusInternalServerError
+		if he, ok := err.(*httpError); ok {
+			return he
+		}
+		if he, ok := err.(interface {
+			StatusCode() int
+		}); ok {
+			code = he.StatusCode()
+		} else {
+			code = http.StatusInternalServerError
+		}
 	}
 	if err == nil {
 		err = errgo.Newf(http.StatusText(code))
 	}
-	return httpError{error: err, code: code}
+	return &httpError{error: err, code: code}
 }
 
 // Newf returns a new httpError with the given code,
 // and the message is created from msg and the args.
-func Newf(code int, msg string, args ...interface{}) httpError {
+func Newf(code int, msg string, args ...interface{}) *httpError {
 	return New(errgo.Newf(msg, args), code)
 }
 
