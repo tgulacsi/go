a0d44c8ccc86fc879d8e1ec4800d961dfb521812
Tamas Gulacsi <tgulacsi78@gmail.com>
2017-09-18 13:07:31 +0200

    use DBMS_SQL.DESCRIBE_QUERY2

--- orahlp/orahlp.go
+++ orahlp/orahlp.go
@@ -8,6 +8,9 @@ package orahlp
 import (
 	"bytes"
 	"database/sql"
+	"io"
+	"strconv"
+	"encoding/csv"
 	"fmt"
 	"strings"
 
@@ -48,23 +51,29 @@ func DescribeQuery(db dber.Execer, qry string) ([]Column, error) {
 	if _, err := db.Exec(`DECLARE
   c INTEGER;
   col_cnt INTEGER;
-  rec_tab DBMS_SQL.DESC_TAB;
-  a DBMS_SQL.DESC_REC;
+  rec_tab DBMS_SQL.DESC_TAB2;
+  a DBMS_SQL.DESC_REC2;
   v_idx PLS_INTEGER;
   res VARCHAR2(32767);
+  FUNCTION escape(s VARCHAR2) RETURN VARCHAR2 IS
+  BEGIN
+    RETURN(REPLACE(s, '"', '""'));
+  END escape;
 BEGIN
   c := DBMS_SQL.OPEN_CURSOR;
   BEGIN
     DBMS_SQL.PARSE(c, :1, DBMS_SQL.NATIVE);
-    DBMS_SQL.DESCRIBE_COLUMNS(c, col_cnt, rec_tab);
+    DBMS_SQL.DESCRIBE_COLUMNS2(c, col_cnt, rec_tab);
     v_idx := rec_tab.FIRST;
     WHILE v_idx IS NOT NULL LOOP
       a := rec_tab(v_idx);
-      res := res||a.col_schema_name||' '||a.col_name||' '||a.col_type||' '||
-                  a.col_max_len||' '||a.col_precision||' '||a.col_scale||' '||
-                  (CASE WHEN a.col_null_ok THEN 1 ELSE 0 END)||' '||
-                  a.col_charsetid||' '||a.col_charsetform||
-                  CHR(10);
+      res := res||a.col_type||','||
+                  a.col_max_len||','||a.col_precision||','||a.col_scale||','||
+                  (CASE WHEN a.col_null_ok THEN 1 ELSE 0 END)||','||
+                  a.col_charsetid||','||a.col_charsetform||',"'||
+				  escape(a.col_schema_name)||'","'||
+				  escape(a.col_name)||'"'||
+				  CHR(10);
       v_idx := rec_tab.NEXT(v_idx);
     END LOOP;
 	--Loop ended, close cursor
@@ -75,35 +84,36 @@ BEGIN
 	RAISE;
   END;
   :2 := UTL_RAW.CAST_TO_RAW(res);
-END;`, qry, &res,
+  END;`, qry, sql.Out{Dest:&res},
 	); err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, qry)
 	}
 	if i := bytes.IndexByte(res, 0); i >= 0 {
 		res = res[:i]
 	}
-	lines := bytes.Split(res, []byte{'\n'})
-	cols := make([]Column, 0, len(lines))
-	var nullable int
-	for _, line := range lines {
-		if len(line) == 0 {
-			continue
+	cr := csv.NewReader(bytes.NewReader(res))
+	cr.FieldsPerRecord = 9
+	cr.ReuseRecord = true
+
+	var cols []Column
+	for {
+		record, err := cr.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return cols, err
 		}
+		var nullable int
 		var col Column
-		switch j := bytes.IndexByte(line, ' '); j {
-		case -1:
-			continue
-		case 0:
-			line = line[1:]
-		default:
-			col.Schema, line = string(line[:j]), line[j+1:]
+		for i, d := range []*int{&col.Type, &col.Length, &col.Precision, &col.Scale, &nullable, &col.CharsetID, &col.CharsetForm}{
+			if *d, err = strconv.Atoi(record[i]); err != nil {
+				return cols, errors.Wrapf(err, "parsing %q (%d.)", d, i)
+			}
 		}
-		if n, err := fmt.Sscanf(string(line), "%s %d %d %d %d %d %d %d",
-			&col.Name, &col.Type, &col.Length, &col.Precision, &col.Scale, &nullable, &col.CharsetID, &col.CharsetForm,
-		); err != nil {
-			return cols, errors.Wrapf(err, "parsing %q (parsed: %d)", line, n)
-		}
-		col.Nullable = nullable != 0
+		col.Nullable = nullable == 1
+		col.Schema, col.Name = record[7], record[8]
+
 		cols = append(cols, col)
 	}
 	return cols, nil
