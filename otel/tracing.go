ce0e46eb7ca8c382deb246f0c8c62abfd47931c5
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2020-08-05 20:33:56 +0200

    otel: HTTP Extract and Inject

--- otel/tracing.go
+++ otel/tracing.go
@@ -3,22 +3,117 @@ package otel
 import (
 	"context"
 	"net/http"
-	"fmt"
 	"strings"
 
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/label"
-	"go.opentelemetry.io/otel/api/trace"
 	"go.opentelemetry.io/otel/api/propagation"
-	"go.opentelemetry.io/otel/exporters/stdout"
-	"go.opentelemetry.io/otel/instrumentation/httptrace"
+	"go.opentelemetry.io/otel/api/trace"
+	setrace "go.opentelemetry.io/otel/sdk/export/trace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/go-logfmt/logfmt"
 )
 
+func InitTraceProvider(ctx context.Context, name string, Log func(...interface{}) error) (context.Context, error) {
+	exporter := LogExporter{Log:Log}
+	tp, err := sdktrace.NewProvider(
+		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+		sdktrace.WithSyncer(exporter),
+	)
+	if err != nil {
+		return ctx, err
+	}
+	global.SetTraceProvider(tp)
+	return context.WithValue(ctx, ckTracer, global.Tracer(name)), nil
+}
+
+var Propagators = propagation.New(
+	propagation.WithExtractors(trace.DefaultHTTPPropagator(), trace.B3{}),
+	propagation.WithInjectors(trace.DefaultHTTPPropagator(), trace.B3{}),
+)
+
+func Middleware(hndl http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := propagation.ExtractHTTP(r.Context(),Propagators, r.Header)
+		FromContext(ctx).WithSpan(ctx, r.URL.Path, func(ctx context.Context) error {
+			propagation.InjectHTTP(ctx, Propagators, w.Header())
+			hndl.ServeHTTP(w, r.WithContext(ctx))
+			return nil
+		})
+	})
+}
+
+func GlobalTracer() trace.Tracer { return global.Tracer("github.com/tgulacsi/go/otel") }
+func FromContext(ctx context.Context) trace.Tracer {
+	if tracer, ok := ctx.Value(ckTracer).(trace.Tracer); ok {
+		return tracer
+	}
+	return GlobalTracer()
+}
+
+type ctxKey string
+
+const ckTracer = ctxKey("tracer")
+
 type logfmtEncoder struct {
 	id label.EncoderID
+}
+
+func NewExportPipeline(Log func(...interface{}) error) (trace.Provider, error) {
+	exporter := LogExporter{Log: Log}
+	return sdktrace.NewProvider(sdktrace.WithSyncer(exporter))
+}
+
+type LogExporter struct {
+	Log func(...interface{}) error
+}
+
+// ExportSpans writes SpanData in json format to stdout.
+func (e LogExporter) ExportSpans(ctx context.Context, data []*setrace.SpanData) {
+	for _, d := range data {
+		e.ExportSpan(ctx, d)
+	}
+}
+
+// ExportSpan writes a SpanData in json format to stdout.
+func (e LogExporter) ExportSpan(ctx context.Context, data *setrace.SpanData) {
+	/*
+	   type SpanData struct {
+	   	SpanContext  apitrace.SpanContext
+	   	ParentSpanID apitrace.SpanID
+	   	SpanKind     apitrace.SpanKind
+	   	Name         string
+	   	StartTime    time.Time
+	   	// The wall clock time of EndTime will be adjusted to always be offset
+	   	// from StartTime by the duration of the span.
+	   	EndTime                  time.Time
+	   	Attributes               []kv.KeyValue
+	   	MessageEvents            []Event
+	   	Links                    []apitrace.Link
+	   	StatusCode               codes.Code
+	   	StatusMessage            string
+	   	HasRemoteParent          bool
+	   	DroppedAttributeCount    int
+	   	DroppedMessageEventCount int
+	   	DroppedLinkCount         int
+
+	   	// ChildSpanCount holds the number of child span created for this span.
+	   	ChildSpanCount int
+
+	   	// Resource contains attributes representing an entity that produced this span.
+	   	Resource *resource.Resource
+
+	   	// InstrumentationLibrary defines the instrumentation library used to
+	   	// providing instrumentation.
+	   	InstrumentationLibrary instrumentation.Library
+	   }
+	*/
+	e.Log("msg", "trace", "parent", data.ParentSpanID, "kind", data.SpanKind, "name", data.Name,
+		"spanID", data.SpanContext.SpanID, "traceID", data.SpanContext.TraceID, "start", data.StartTime, "end", data.EndTime,
+		"attrs", data.Attributes, "events", data.MessageEvents, "links", data.Links,
+		"statusCode", data.StatusCode, "statusMsg", data.StatusMessage,
+	)
 }
 
 func NewLogfmtEncoder() logfmtEncoder {
@@ -35,56 +130,3 @@ func (e logfmtEncoder) Encode(it label.Iterator) string {
 	enc.EndRecord()
 	return buf.String()
 }
-
-type logWriter struct{ Log func(...interface{}) error }
-
-func (lw logWriter) Write(p []byte) (int, error) {
-	err := lw.Log("trace", string(p))
-	return len(p), err
-}
-
-func InitTraceProvider(ctx context.Context, name string, Log func(...interface{}) error) (context.Context, error) {
-	exporter, err := stdout.NewExporter(
-		stdout.WithWriter(logWriter{Log: Log}),
-		stdout.WithLabelEncoder(NewLogfmtEncoder()),
-		stdout.WithoutTimestamps(), stdout.WithoutMetricExport(),
-	)
-	if err != nil {
-		return ctx, err
-	}
-	tp, err := sdktrace.NewProvider(
-		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
-		sdktrace.WithSyncer(exporter),
-	)
-	if err != nil {
-		return ctx, err
-	}
-	global.SetTraceProvider(tp)
-	return context.WithValue(ctx, ckTracer, global.Tracer(name)), nil
-}
-
-var Propagators = propagation.New(
-	propagation.WithExtractors(trace.DefaultHTTPPropagator()),
-	propagation.WithInjectors(trace.DefaultHTTPPropagator()),
-)
-var httptraceOpts = []httptrace.Option{httptrace.WithPropagators(Propagators)}
-
-func Middleware(hndl http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tracer, ok := r.Context().Value(ckTracer).(trace.Tracer)
-		if !ok || tracer == nil {
-			fmt.Println("NO TRACER IN CONTEXT")
-			hndl.ServeHTTP(w, r)
-			return
-		}
-		tracer.WithSpan(r.Context(), r.URL.Path, func(ctx context.Context) error {
-			_, _, spanContext := httptrace.Extract(ctx, r, httptraceOpts...)
-			hndl.ServeHTTP(w, r.WithContext(trace.ContextWithRemoteSpanContext(ctx, spanContext)))
-			return nil
-		})
-	})
-}
-
-type ctxKey string
-
-const ckTracer = ctxKey("tracer")
