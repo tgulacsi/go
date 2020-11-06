c1cd7673dd7cfbc60baa920ba0586cfa6bf2e19e
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2020-11-06 08:56:30 +0100

    httpunix: Raise timeouts in ListenAndServe

--- httpunix/listen.go
+++ httpunix/listen.go
@@ -33,11 +33,36 @@ import (
 // ListenAndServe is the same as http.ListenAndServe, except it can listen on unix domain sockets.
 func ListenAndServe(ctx context.Context, addr string, hndl http.Handler) error {
 	return ListenAndServeSrv(ctx, addr, http.Server{
-		Handler:           hndl,
+		Handler: hndl,
+
+		// ReadTimeout is the maximum duration for reading the entire
+		// request, including the body.
+		//
+		// Because ReadTimeout does not let Handlers make per-request
+		// decisions on each request body's acceptable deadline or
+		// upload rate, most users will prefer to use
+		// ReadHeaderTimeout. It is valid to use them both.
+		ReadTimeout: time.Minute,
+
+		// ReadHeaderTimeout is the amount of time allowed to read
+		// request headers. The connection's read deadline is reset
+		// after reading the headers and the Handler can decide what
+		// is considered too slow for the body. If ReadHeaderTimeout
+		// is zero, the value of ReadTimeout is used. If both are
+		// zero, there is no timeout.
 		ReadHeaderTimeout: 15 * time.Second,
-		ReadTimeout:       30 * time.Second,
-		WriteTimeout:      60 * time.Second,
-		IdleTimeout:       120 * time.Second,
+
+		// WriteTimeout is the maximum duration before timing out
+		// writes of the response. It is reset whenever a new
+		// request's header is read. Like ReadTimeout, it does not
+		// let Handlers make decisions on a per-request basis.
+		WriteTimeout: time.Hour,
+
+		// IdleTimeout is the maximum amount of time to wait for the
+		// next request when keep-alives are enabled. If IdleTimeout
+		// is zero, the value of ReadTimeout is used. If both are
+		// zero, there is no timeout.
+		IdleTimeout: 5 * time.Minute,
 	})
 }
 
