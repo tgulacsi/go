30017d464a120e25d78db3fa96e84adc09a3d7ba
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2023-12-13 08:57:50 +0100

    pdfcpu v0.6.0

--- pdf/pdf.go
+++ pdf/pdf.go
@@ -79,7 +79,7 @@ func MergeFiles(ctx context.Context, dest string, sources ...string) (err error)
 		return destfh.CloseAtomicallyReplace()
 	}
 
-	if err = api.MergeAppendFile(sources, dest, config); err == nil {
+	if err = api.MergeAppendFile(sources, dest, false, config); err == nil {
 		size := fileSize(dest)
 		if size > 5 {
 			return nil
