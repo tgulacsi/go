6f9af10e43a45a537707a85cfbc4b534eeead17a
Tamás Gulácsi <tamas@gulacsi.eu>
2026-07-20 14:30:17 +0200

    secret: add switch to toggle garbling

--- secret/password.go
+++ secret/password.go
@@ -7,37 +7,65 @@ package secret
 import (
 	"bytes"
 	"strings"
+	"sync/atomic"
 
 	"github.com/go-json-experiment/json"
 	"github.com/go-json-experiment/json/jsontext"
 )
+
+// MarshalPassword allows overriding the default *** behaviour.
+var MarshalPassword atomic.Bool
 
 // Password is a string that renders to *** in XML/JSON (MarshalText/<arshalJSON)
 type Password string
 
 // MarshalText returns ***.
 func (passw Password) MarshalText() ([]byte, error) {
+	if MarshalPassword.Load() {
+		return []byte(passw), nil
+	}
 	return bytes.Repeat([]byte("*"), len(passw)), nil
+}
+
+// UnmarshalJSON reads the text.
+// Watch out that this fails for itself (*** -marshaled text)!
+func (passw *Password) UnmarshalText(p []byte) error {
+	if bytes.IndexFunc(p, func(r rune) bool { return r != '*' }) < 0 {
+		return nil
+	}
+	*passw = Password(p)
+	return nil
 }
 
 // MarshalJSONTo encodes ***.
 func (passw Password) MarshalJSONTo(enc *jsontext.Encoder) error {
+	if MarshalPassword.Load() {
+		return enc.WriteToken(jsontext.String(string(passw)))
+	}
 	return enc.WriteToken(jsontext.String(strings.Repeat("*", len(passw))))
 }
 
 // MarshalJSON returns ***.
 func (passw Password) MarshalJSON() ([]byte, error) {
 	p := append(make([]byte, 0, 1+len(passw)+1), '"')
-	for range len(passw) {
-		p = append(p, '*')
+	if MarshalPassword.Load() {
+		p = append(p, []byte(passw)...)
+	} else {
+		for range len(passw) {
+			p = append(p, '*')
+		}
 	}
 	return append(p, '"'), nil
 }
 
 // UnmarshalJSON reads the JSON.
+// Watch out that this fails for itself (*** -marshaled JSON)!
 func (passw *Password) UnmarshalJSON(p []byte) error {
 	var s string
 	err := json.Unmarshal(p, &s)
+	if strings.IndexFunc(s, func(r rune) bool { return r != '*' }) < 0 {
+		return nil
+	}
 	*passw = Password(s)
 	return err
 }
